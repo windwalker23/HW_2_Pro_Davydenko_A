5f4eda9a212c1a9cba53ca7a9fc28c5717b822d4
unknown <yeskennyt@gmail.com>
2022-06-01 15:27:45 +0300

    initial-commit

---
+++ script.js
@@ -0,0 +1,14 @@
+R = 10;
+
+H = 20;
+
+S = Math.pow(R, 2) * Math.PI;
+
+V = S * H;
+
+console.log("*********************")
+console.log("Объем цилиндра с площадью основы S =", S, "радиусом R =", R, "и высотой H =", H, "равен:")
+console.log("---------------------")
+console.log("V =", V)
+console.log("---------------------")
+console.log("end.")
