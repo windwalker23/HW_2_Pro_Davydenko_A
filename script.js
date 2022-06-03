4ce97693d1e2f1eb89f38a32fc4cfb35a6d3f11e
unknown <yeskennyt@gmail.com>
2022-06-04 00:08:56 +0300

    v1.0

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
-R = 10;
+firstName = prompt("Please enter your first name here.")
+    .trim();
+firstNamelenght = firstName.lenght;
+firstName = `${firstName[0].toUpperCase()}${firstName.slice(1, firstNamelenght)}`;
 
-H = 20;
+lastName = prompt("Please enter your last name here.")
+    .trim();
+    lastNamelenght = lastName.lenght;
+    lastName = `${lastName[0].toUpperCase()}${lastName.slice(1, lastNamelenght)}`;
 
-S = Math.pow(R, 2) * Math.PI;
+yearBirth = parseInt(prompt("Please enter your year of birth.")
+    .replaceAll(" ", ""));
+today = new Date();
+yearNow = today.getFullYear();
+age = yearNow - yearBirth;
 
-V = S * H;
+email = prompt("Please enter your email.")
+    .replaceAll(" ", "")
+    .toLowerCase();
 
-console.log("*********************")
-console.log("Объем цилиндра с площадью основы S =", S, "радиусом R =", R, "и высотой H =", H, "равен:")
-console.log("---------------------")
-console.log("V =", V)
-console.log("---------------------")
-console.log("end.")
+if(email.indexOf(`@`) == -1){
+    message = `not valid email <b>${email}</b> (symbol @ not exist)`;
+} else if(!email.indexOf(`@`)){
+    message = `not valid email <b>${email}</b> (symbol @ find in first place)`;
+} else if(email.indexOf(`@`) == email.length-1){
+    message = `not valid email <b>${email}</b> (symbol @ find in last place)`;
+} else {
+    message = `<b>${email}</b>`;
+}
+
+document.write(`<ul>
+                    <li>Full name: <b>${firstName}</b> <b>${lastName}</b></li>
+                    <li>Email: ${message}</li>
+                    <li>Age: <b>${age}</b></li>
+                </ul>`);
